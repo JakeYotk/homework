0a35f41ceca3d42a0f11b844c31a85bd27be23b6
Jacob <yo.yashkins@gmail.com>
2022-09-25 00:32:46 +0700

    вторая задача

--- HomeWork02/Ex02/Program.cs
+++ HomeWork02/Ex02/Program.cs
@@ -6,3 +6,18 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+
+if (number.Length > 2)
+
+{
+   Console.WriteLine($"третья цифра - {number[2]}"); 
+}
+
+else 
+
+{
+    Console.WriteLine("Третьей цифры нет");
+}
