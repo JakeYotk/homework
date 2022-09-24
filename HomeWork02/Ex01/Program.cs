f713ba59aaa07de1194280c3401a642fa46b1a26
Jacob <yo.yashkins@gmail.com>
2022-09-24 23:26:56 +0700

    пробую варианты решения

--- HomeWork02/Ex01/Program.cs
+++ HomeWork02/Ex01/Program.cs
@@ -8,12 +8,15 @@
 // Описать метод нахождения второй цифры числа 
 // Описать метод вывода искомой цифры
 
-int num = new Random().Next(100, 999);
-Console.WriteLine(num);
-int i = 0;
-int result = num;
-while (i < 1)
-{
-    i++;
-}
-Console.WriteLine(i = 1);
+// int num = new Random().Next(100, 999);
+// Console.WriteLine(num);
+// int i = 0;
+// int result = num;
+// while (i < 1)
+// {
+//     i++;
+// }
+// Console.WriteLine(i = 1);
+
+string num = 1234.ToString();
+Console.WriteLine(num[1]);
