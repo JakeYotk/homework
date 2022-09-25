879b0dc9feff9b7ab2ebdeb48b67dc6ae2777fb5
Jacob <yo.yashkins@gmail.com>
2022-09-25 15:09:51 +0700

    create Trash

---
+++ Trash/pomoyka/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+
+if (number.Length > 2)
+{
+   Console.WriteLine($"третья цифра - {number[2]}"); 
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
